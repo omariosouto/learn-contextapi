37aaebfeab097517d9938ce9200e445685500811
Mario Souto <soutomarios@gmail.com>
2019-05-21 15:03:50 -0300

    update notification context

--- src/contexts/NotificationContext.js
+++ src/contexts/NotificationContext.js
@@ -1,21 +1,21 @@
 // Criar um context de notificações
 import React, { useState, createContext } from "react";
-import "../assets/notification.css";
+import "../assets/notificacao.css";
 
 export const NotificationContext = createContext({
   msg: "",
   setMsg(value) {}
 });
 
-export const NotificationContextProvider = () => {
+export const NotificationContextProvider = ({ children }) => {
   const [msg, setMsg] = useState("");
 
   return (
     <NotificationContext.Provider value={{ msg, setMsg }}>
-      {this.props.children}
-      {this.state.msg && (
-        <div className="notificacaoMsg" onAnimationEnd={this.clearMsg}>
-          {this.state.msg}
+      {children}
+      {msg && (
+        <div className="notificacaoMsg" onAnimationEnd={() => setMsg("")}>
+          {msg}
         </div>
       )}
     </NotificationContext.Provider>
