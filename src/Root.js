24836c2ca391ee12612b21ea6742585dd17c178a
Mario Souto <soutomarios@gmail.com>
2019-05-22 14:48:40 -0300

    add contextapi

--- src/Root.js
+++ src/Root.js
@@ -1,42 +1,54 @@
-import React, { Component } from "react";
+import React, { Component, createContext, useContext } from "react";
 import "./assets/bootstrap.min.css";
+import {
+  NotificationContextProvider,
+  NotificationContext
+} from "./contexts/NotificationContext";
+
+// ContextAPI
+const TemaContext = createContext("");
 
 // [Quem quer o ${theme}]
-class Card extends Component {
-  render() {
-    const { title, theme } = this.props;
-
-    return (
-      <div className={`card`}>
-        <div className={`card-header bg-${theme} text-white`}>
-          <h5>{title}</h5>
-        </div>
-        <img
-          className="card-img-top"
-          src="https://placehold.it/280x150"
-          alt="Card cap"
-        />
-        <div className="card-body">
-          <p className="card-text">lorem ipsum dolor sit amet bla aauh asuh</p>
-          <a href="/" className={`btn btn-${theme}`}>
-            Go somewhere
-          </a>
-        </div>
+const Card = ({ title }) => {
+  const theme = useContext(TemaContext);
+  return (
+    <div className={`card`}>
+      <NotificationContext.Consumer>
+        {context => (
+          <button onClick={() => context.setMsg("alo alo wbrazil")}>
+            Dispara notificação
+          </button>
+        )}
+      </NotificationContext.Consumer>
+      <div className={`card-header bg-${theme} text-white`}>
+        <h5>
+          {title} {theme}
+        </h5>
       </div>
-    );
-  }
-}
+      <img
+        className="card-img-top"
+        src="https://placehold.it/280x150"
+        alt="Card cap"
+      />
+      <div className="card-body">
+        <p className="card-text">lorem ipsum dolor sit amet bla aauh asuh</p>
+        <a href="/" className={`btn btn-${theme}`}>
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
+};
 
 // [Intermediário]
 class HomePage extends Component {
   render() {
-    const { theme } = this.props;
     return (
       <main className="container">
         <div className="row">
           {["#01 - Card", "#02 - Card", "#03 - Card"].map(cardText => (
             <div className="col-6 mb-4" key={cardText}>
-              <Card title={cardText} theme={theme} />
+              <Card title={cardText} />
             </div>
           ))}
         </div>
@@ -53,26 +65,30 @@ class Root extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <HomePage theme={this.state.theme} />
+      <NotificationContextProvider>
+        <React.Fragment>
+          <TemaContext.Provider value={this.state.theme}>
+            <HomePage />
+          </TemaContext.Provider>
 
-        <select
-          style={{
-            position: "fixed",
-            bottom: "5%",
-            right: "5%",
-            width: "250px",
-            height: "50px"
-          }}
-          onChange={({ target }) => {
-            this.setState({ theme: target.value });
-          }}
-        >
-          <option value="primary">Azul</option>
-          <option value="dark">Escuro</option>
-          <option value="danger">Vermelho</option>
-        </select>
-      </React.Fragment>
+          <select
+            style={{
+              position: "fixed",
+              bottom: "5%",
+              right: "5%",
+              width: "250px",
+              height: "50px"
+            }}
+            onChange={({ target }) => {
+              this.setState({ theme: target.value });
+            }}
+          >
+            <option value="primary">Azul</option>
+            <option value="dark">Escuro</option>
+            <option value="danger">Vermelho</option>
+          </select>
+        </React.Fragment>
+      </NotificationContextProvider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import Root from "./Root";
+// import Root from "./Root";
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+// useEffect(() => {
+//     console.log('didMount')
+
+//     return () => {
+//         console.log('willUnmount')
+//     }
+// }, [])
+// class Contador {
+//   state = {
+//     valorDoContador: 0
+//   };
+//   componentDidMount() {
+//     console.log("didMount");
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         Valor: {this.state.valorDoContador}
+//         <button
+//           onClick={() =>
+//             this.setState({ valorDoContador: this.state.valorDoContador + 1 })
+//           }
+//         >
+//           Incrementar
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+const Contador = () => {
+  const [valorDoContador, setValorDoContador] = useState(0);
+
+  function fetchData() {
+    setTimeout(() => {
+      setValorDoContador(50);
+    }, 10000);
+  }
+
+  // Usar lifecycle em componentes de função
+  // https://github.com/acdlite/recompose
+  // Ajuda quando vc precisa saber quando atualiza ou não um dado
+  useEffect(() => {
+    console.log("didMount");
+    fetchData();
+
+    return () => {
+      console.log("willUnMount");
+    };
+  }, []);
+
+  return (
+    <div>
+      Valor: {valorDoContador}
+      <button onClick={() => setValorDoContador(valorDoContador + 1)}>
+        Incrementar
+      </button>
+    </div>
+  );
+};
+
+ReactDOM.render(<Contador />, document.getElementById("root"));
